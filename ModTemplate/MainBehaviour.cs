16d4da2094bec1d5afe9b5cda4d90351482bc379
Vesper <0ooechooo0oooo@gmail.com>
2021-10-25 16:51:26 +0100

    First full release!

---
+++ ModTemplate/MainBehaviour.cs
@@ -0,0 +1,123 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using OWML.ModHelper.Events;
+using UnityEngine;
+
+namespace VespersAssortedOuterWildsShaders
+{
+    public class MainBehaviour : ModBehaviour
+    {
+        RenderTexture cameraTexture;
+        RenderTexture customTexture;     
+        Camera customCamera;
+
+        public float ASCIIScaleFactor;
+        public float ASCIIBackBrightness;
+
+        public float PixelScaleFactor;
+        public bool PixelNoBlend;
+
+        public int OilIntensity;
+        public int OilRadius;
+
+        public bool PixelShaderOn;
+        public bool ASCIIShaderOn;
+        public bool OilShaderOn;
+
+        public ComputeShader ASCIIShader;
+        public ComputeShader PixelShader;
+        public ComputeShader OilShader;
+
+        public bool AllShadersOff { get => !ASCIIShaderOn && !PixelShaderOn && !OilShaderOn; }
+        Camera NormalCamera { get => Camera.main; }
+        public static MainBehaviour Instance { get; set; }
+        private void Start()
+        {
+            Instance = this;
+
+            //ModHelper.Console.WriteLine("Skipping splash screen...");
+            //var titleScreenAnimation = FindObjectOfType<TitleScreenAnimation>();
+            //titleScreenAnimation.SetValue("_fadeDuration", 0);
+            //titleScreenAnimation.SetValue("_gamepadSplash", false);
+            //titleScreenAnimation.SetValue("_introPan", false);
+            //titleScreenAnimation.Invoke("FadeInTitleLogo");
+            //ModHelper.Console.WriteLine("Done!");
+
+            ModHelper.Events.Scenes.OnCompleteSceneChange += OnCompleteSceneChange;
+
+            Textures();
+            ComputeShaders();
+            CameraWork();
+        }
+
+        private void OnCompleteSceneChange(OWScene oldScene, OWScene newScene)
+        {
+            CameraWork();
+        }
+
+        public override void Configure(IModConfig config)
+        {
+            PixelShaderOn = config.GetSettingsValue<bool>("Pixel shader");
+            ASCIIShaderOn = config.GetSettingsValue<bool>("ASCII shader");
+            OilShaderOn = config.GetSettingsValue<bool>("Oil shader");
+
+            PixelNoBlend = !config.GetSettingsValue<bool>("Pixel blend");
+            PixelScaleFactor = config.GetSettingsValue<float>("Pixel scale");
+            ASCIIScaleFactor = config.GetSettingsValue<float>("ASCII scale");
+            ASCIIBackBrightness = config.GetSettingsValue<float>("ASCII back brightness");
+            OilIntensity = config.GetSettingsValue<int>("Oil intensity");
+            OilRadius = config.GetSettingsValue<int>("Oil radius");
+        }
+
+        private void Textures()
+        {
+            ModHelper.Console.WriteLine("Creating textures...");
+            customTexture = new RenderTexture(1920, 1080, 0);
+            customTexture.enableRandomWrite = true;
+            customTexture.filterMode = FilterMode.Point;
+            customTexture.Create();
+
+            cameraTexture = new RenderTexture(1920, 1080, 0);
+            cameraTexture.enableRandomWrite = true;
+            cameraTexture.filterMode = FilterMode.Point;
+            cameraTexture.Create();
+            ModHelper.Console.WriteLine("Done!");
+        }
+
+        private void ComputeShaders()
+        {
+            ModHelper.Console.WriteLine("Loading compute shaders...");
+
+            var shaderbundle = ModHelper.Assets.LoadBundle("shaders");
+
+            ASCIIShader = shaderbundle.LoadAsset<ComputeShader>("ASCIIShader");
+            PixelShader = shaderbundle.LoadAsset<ComputeShader>("PixelShader");
+            OilShader = shaderbundle.LoadAsset<ComputeShader>("OilShader");
+            ModHelper.Console.WriteLine("Done!");
+        }
+
+        private void CameraWork()
+        {
+            if (NormalCamera == null) { return; }
+
+            GameObject textureCameraGO = new GameObject();
+            customCamera = textureCameraGO.AddComponent<Camera>();
+            textureCameraGO.AddComponent<OWCamera>();
+
+            textureCameraGO.transform.SetParent(NormalCamera.transform);
+            textureCameraGO.transform.position = Vector3.zero;
+            textureCameraGO.transform.rotation = Quaternion.identity;
+            customCamera.CopyFrom(NormalCamera);
+            customCamera.cullingMask = 1 << 5;
+            NormalCamera.targetTexture = cameraTexture;
+            ModHelper.Console.WriteLine("Done!");
+
+            AddCustomRenderingToCamera(textureCameraGO);
+
+        }
+        private void AddCustomRenderingToCamera(GameObject camera)
+        {
+            camera.AddComponent<ShaderApplier>().Ready(cameraTexture, customTexture);
+        }
+    }
+}
---
+++ ModTemplate/ShaderApplier.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace VespersAssortedOuterWildsShaders
+{
+    class ShaderApplier : MonoBehaviour
+    {
+        RenderTexture cameraTexture;
+        RenderTexture customTexture;
+        bool running;
+       
+        public void Ready(RenderTexture cameraTexture, RenderTexture customTexture)
+        {
+            this.cameraTexture = cameraTexture;
+            this.customTexture = customTexture;
+            running = true;
+        }
+        private void Update()
+        {
+            if (!running || Keyboard.current["h"].IsPressed()) { return; }
+
+            if (MainBehaviour.Instance.ASCIIShaderOn)
+            {
+                MainBehaviour.Instance.ASCIIShader.SetTexture(0, "cameraTexture", cameraTexture);
+                MainBehaviour.Instance.ASCIIShader.SetTexture(0, "asciiTexture", customTexture);
+                MainBehaviour.Instance.ASCIIShader.SetFloat("scaleFactor", MainBehaviour.Instance.ASCIIScaleFactor);
+                MainBehaviour.Instance.ASCIIShader.SetFloat("backBrightness", MainBehaviour.Instance.ASCIIBackBrightness);
+                MainBehaviour.Instance.ASCIIShader.Dispatch(0, 1920 / (8 * 8), 1100 / (8 * 8), 1);
+            }
+            if (MainBehaviour.Instance.PixelShaderOn)
+            {
+                MainBehaviour.Instance.PixelShader.SetTexture(0, "cameraTexture", cameraTexture);
+                MainBehaviour.Instance.PixelShader.SetTexture(0, "pixelTexture", customTexture);
+                MainBehaviour.Instance.PixelShader.SetFloat("scaleFactor", MainBehaviour.Instance.PixelScaleFactor);
+                MainBehaviour.Instance.PixelShader.SetBool("noBlend", MainBehaviour.Instance.PixelNoBlend);
+                MainBehaviour.Instance.PixelShader.Dispatch(0, 1920 / (int)Math.Min(MainBehaviour.Instance.PixelScaleFactor * MainBehaviour.Instance.PixelScaleFactor, 8 * 8),
+                    1100 / (int)Math.Min(MainBehaviour.Instance.PixelScaleFactor * MainBehaviour.Instance.PixelScaleFactor, 8 * 8), 1);
+            }
+            if (MainBehaviour.Instance.OilShaderOn)
+            {
+                MainBehaviour.Instance.OilShader.SetTexture(0, "inputTexture", cameraTexture);
+                MainBehaviour.Instance.OilShader.SetTexture(0, "outputTexture", customTexture);
+                MainBehaviour.Instance.OilShader.SetInt("radius", MainBehaviour.Instance.OilRadius);
+                MainBehaviour.Instance.OilShader.SetInt("intensity", MainBehaviour.Instance.OilIntensity);
+                MainBehaviour.Instance.OilShader.Dispatch(0, 1920 / 32, 1100 / 32, 1);
+            }
+
+        }
+
+        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (!running)
+            {
+                return;
+            }
+            if (Keyboard.current["h"].IsPressed() || MainBehaviour.Instance.AllShadersOff)
+            {
+                Graphics.Blit(cameraTexture, destination);
+            }
+            else
+            {
+                Graphics.Blit(customTexture, destination);
+            }
+
+        }
+    }
+}
